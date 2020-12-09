794f661e15165be9cbd9cf39649beea04d848f26
MickeyKen <mickey333ff@gmail.com>
2020-12-09 12:58:52 +0900

    [Edit] find object_name

--- leg_detector/scripts/original_filtering_leg_detector.py
+++ leg_detector/scripts/original_filtering_leg_detector.py
@@ -10,15 +10,19 @@ import message_filters
 from people_msgs.msg import PositionMeasurement, PositionMeasurementArray
 from geometry_msgs.msg import PoseStamped, PointStamped
 import math
+import tf
+from geometry_msgs.msg import Quaternion
 
-SEARCH_RADIUS = 0.75
+SEARCH_RADIUS = 0.25
+
 def callback(leg, people, area):
-    rospy.loginfo("leg timestamp: %d ns" % leg.header.stamp.to_nsec())
-    rospy.loginfo("people timestamp: %d ns" % people.header.stamp.to_nsec())
-    rospy.loginfo("area timestamp: %d ns" % area.header.stamp.to_nsec())
+    # rospy.loginfo("leg timestamp: %d ns" % leg.header.stamp.to_nsec())
+    # rospy.loginfo("people timestamp: %d ns" % people.header.stamp.to_nsec())
+    # rospy.loginfo("area timestamp: %d ns" % area.header.stamp.to_nsec())
 
     center_x = area.point.x
     center_y = area.point.y
+    print ("X: ", center_x, "Y: ", center_y)
 
     target_name = ""
     target_diff = SEARCH_RADIUS
@@ -27,18 +31,50 @@ def callback(leg, people, area):
 
 
     for p in people.people:
-        diff = math.hypot(center_x - p.pos.x, center_y - p.pos.y)
-        if diff < SEARCH_RADIUS and diff < target_diff:
-            target_diff = diff
-            target_name = p.object_id
-            posearray_msg.pos.position.x = p.pos.x
-            posearray_msg.pos.position.y = p.pos.y
+        if p.pos.y > 0.0:
+            diff = math.hypot(center_x - p.pos.x, center_y - p.pos.y)
+            print ("postition: ", p.pos.x, p.pos.y)
+            if diff < SEARCH_RADIUS and diff < target_diff:
+                target_diff = diff
+                target_name = p.object_id
+                posearray_msg.pose.position.x = p.pos.x
+                posearray_msg.pose.position.y = p.pos.y
+
 
     if target_name:
+        leg_name0 = target_name.split('|')[0]
+        leg_name1 = target_name.split('|')[1]
+        print ("name: ",leg_name0, leg_name1)
 
-        leg = [[l.pos.x, l.pos.y] for l in leg.people if l.object_id == target_name]
+        legs = [[l.pos.x, l.pos.y] for l in leg.people if l.object_id == leg_name0 or l.object_id == leg_name1]
 
-        if len(leg) == 2:
+        print ("length: ",len(legs))
+
+        if len(legs) == 2:
+
+            rad = math.atan2(legs[1][1]-legs[0][1], legs[1][0]-legs[0][0])
+            deg = math.degrees(rad)
+            deg1 = (deg + 90) % 360
+            deg2 = (deg + 270) % 360
+            if deg1 > 180:
+                q = euler_to_quaternion(deg1)
+                posearray_msg.pose.orientation.x = q.x
+                posearray_msg.pose.orientation.y = q.y
+                posearray_msg.pose.orientation.z = q.z
+                posearray_msg.pose.orientation.w = q.w
+            elif deg2 > 180:
+                q = euler_to_quaternion(deg2)
+                posearray_msg.pose.orientation.x = q.x
+                posearray_msg.pose.orientation.y = q.y
+                posearray_msg.pose.orientation.z = q.z
+                posearray_msg.pose.orientation.w = q.w
+            else:
+                posearray_msg.pose.orientation.x = 0.0
+                posearray_msg.pose.orientation.y = 0.0
+                posearray_msg.pose.orientation.z = 0.0
+                posearray_msg.pose.orientation.w = 1.0
+
+
 
             posearray_msg.header.frame_id = "/base_scan"
             posearray_msg.header.stamp = rospy.Time.now()
@@ -49,9 +85,18 @@ def callback(leg, people, area):
     else:
         pass
 
+def euler_to_quaternion(yaw):
+    """Convert Euler Angles to Quaternion
+
+    euler: geometry_msgs/Vector3
+    quaternion: geometry_msgs/Quaternion
+    """
+    q = tf.transformations.quaternion_from_euler(0, 0, math.radians(yaw))
+    return Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+
 rospy.init_node('filetering_leg_detector_node')
 
-pub = rospy.Publisher("/output", PoseStamped, queue_size=1)
+# pub = rospy.Publisher("/output", PoseStamped, queue_size=1)
 pose_pub = rospy.Publisher("/filtering/scan/detect_leg_person", PoseStamped, queue_size=1)
 sub1 = message_filters.Subscriber("leg_tracker_measurements", PositionMeasurementArray, queue_size=1)
 sub2 = message_filters.Subscriber("people_tracker_measurements", PositionMeasurementArray, queue_size=1)
